57a94620c4289e50be55d17bff0a61b5f7cfc8b9
Gaurav kedia <Gaurav.kedia18@gmail.com>
2018-10-27 07:42:39 +0530

    updated version

--- app/src/main/java/com/example/lenovo/myprofile/MainActivity.kt
+++ app/src/main/java/com/example/lenovo/myprofile/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.lenovo.myprofile
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
+    var ButtonPhoto: ImageView?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.homescreen)
+        ButtonPhoto = findViewById<ImageView>(R.id.profilePhoto)
+        ButtonPhoto?.setOnClickListener({
+            val intent = Intent(this, ProfilePhoto::class.java)
+            startActivity(intent)
+        })
     }
 }
